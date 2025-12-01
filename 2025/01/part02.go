2534ca6757fda184e18fc5c1dbae7c87d66960f6
krusty <krusty@vxn.dev>
2025-12-01 19:40:47 +0100

    Add 01/part2 (wip)

---
+++ 2025/01/part02.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const dialSize = 100
+
+func main() {
+	fhdl, err := os.Open("input02.txt")
+	if err != nil {
+		panic("no input file")
+	}
+	defer func() {
+		err := fhdl.Close()
+		if err != nil {
+			panic("cannot close the file handler")
+		}
+	}()
+
+	var (
+		counter  int
+		position = 50
+	)
+
+	scanner := bufio.NewScanner(fhdl)
+	for scanner.Scan() {
+		bb := scanner.Bytes()
+		op := 1
+
+		distRaw := string(bb[1:])
+		dist, err := strconv.Atoi(distRaw)
+		if err != nil {
+			panic("cannot read the instruction")
+		}
+
+		switch string(bb[0]) {
+		case "L":
+			{
+				op = -1
+				if position-dist < 0 && position != 0 {
+					counter++
+				}
+				if position-dist <= -dialSize {
+					counter -= (position - dist) / dialSize
+				}
+			}
+		case "R":
+		default:
+			{
+				op = 1
+				if position+dist >= dialSize && position != 0 {
+					counter += (position + dist) / dialSize
+				}
+			}
+		}
+
+		position += op * dist
+		if position < 0 {
+			position = dialSize - op*position
+		}
+		position %= dialSize
+
+		if position == 0 {
+			counter++
+		}
+	}
+
+	fmt.Printf("counter: %d\n", counter)
+}
