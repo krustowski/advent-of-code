df4c226531607f7e53ef911fa9a56087cbe28b74
krusty <krusty@vxn.dev>
2025-12-02 18:31:05 +0100

    Update 2025/02/part02 file (wip)

--- 2025/02/part02.go
+++ 2025/02/part02.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -50,24 +52,22 @@ func checkIdRanges(lower, upper int) int {
 		id := strconv.Itoa(num)
 
 		if found, matches := inspectSlice(id); found {
-			// count += i
+			if matches < 2 {
+				continue
+			}
+
 			nums[i] = make([]int, 2)
 			nums[i][0] = num
 			nums[i][1] = matches
+
+			count += num
 
 			if matches > max {
 				max = matches
 			}
 
+			fmt.Printf("%d: \t %d\n", num, matches)
 			i++
-		}
-	}
-
-	for _, val := range nums {
-		if len(val) == 2 && val[1] == max {
-			count = val[0]
-
-			fmt.Printf("%d: \t %d\n", val[0], val[1])
 		}
 	}
 
@@ -75,41 +75,104 @@ func checkIdRanges(lower, upper int) int {
 }
 
 func inspectSlice(str string) (bool, int) {
+	buf := []rune(str[:len(str)/2])
+	matches := 0
+
+	for i := 0; i < len(str)/2; i++ {
+		rgx, err := regexp.Compile(string(buf))
+		if err != nil {
+			fmt.Println(err)
+			return false, 0
+		}
+
+		res := rgx.FindAllString(str, -1)
+		if res == nil {
+			return false, 0
+		}
+
+		if len(res) > 1 && len(str)%len(res) == 0 {
+			if len(buf)*len(res) == len(str) {
+				matches = int(math.Max(float64(matches), float64(len(res))))
+			}
+		}
+
+		if len(buf) > 1 {
+			buf = buf[:len(buf)-1]
+
+			// fmt.Printf("buf len: %d\n", len(buf))
+			continue
+		}
+	}
+
+	return true, matches
+}
+
+func inspectSliceOld(str string) (bool, int) {
 	buf := make([]rune, 0, len(str))
 	matches := 0
 
-	for j, r := range str {
-		if j == 0 {
+	for i, r := range str {
+		if i == 0 {
 			buf = append(buf, r)
 			continue
 		}
 
-		if j+len(buf) <= len(str) && j-len(buf) >= 0 {
-			if str[j-1] == str[j] {
-				buf = append(buf, r)
+		if i == len(str)-1 {
+			if subInspectSlice(string(str[len(str)-1]), len(buf)-1, buf) {
 				matches++
-				continue
+				break
 			}
+		}
 
-			if str[j:j+len(buf)] == str[j-len(buf):j] {
-				buf = append(buf, r)
-				matches++
-				continue
-			}
-
-			if j+len(buf) <= len(str) && string(buf) == str[j:j+len(buf)] {
-				buf = append(buf, r)
-				matches++
-				continue
-			}
-
+		if subInspectSlice(str[i:], i-1, buf) {
+			matches++
+		} else {
 			buf = append(buf, r)
 		}
 	}
 
 	if matches > 0 && len(str)%(matches) == 0 {
+		fmt.Printf("%v\n", string(buf))
 		return true, matches
 	}
 
 	return false, 0
+}
+
+func subInspectSlice(str string, offset int, trend []rune) bool {
+	if len(str) == 0 {
+		return false
+	}
+
+	if len(str) == 1 && offset < len(trend) {
+		if string(str[0]) == string(trend[offset]) {
+			return true
+		}
+	}
+
+	if len(str)%2 == 0 {
+		if len(trend) == 2 {
+			if string(trend) == string(str) {
+				return true
+			}
+		}
+
+		if subInspectSlice(str[1:], offset, trend) {
+			return true
+		}
+	}
+
+	if len(str) > 2 {
+		if len(str)%len(trend) == 0 {
+			if subInspectSlice(str[3:], offset, trend) {
+				return true
+			}
+		}
+
+		if subInspectSlice(str[1:], offset+1, trend) {
+			return true
+		}
+	}
+
+	return false
 }
